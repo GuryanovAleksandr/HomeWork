82779197fb0a31afe93e26c21a2a5d3e0bac9c23
Guryanov <sanyaguru@gmail.com>
2023-09-24 18:54:55 +0800

    add task 04

--- task04/Program.cs
+++ task04/Program.cs
@@ -3,3 +3,15 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число c: ");
+int c = int.Parse(Console.ReadLine()!);
+int Max = a;
+if (a > Max) Max = a;
+if (b > Max) Max = b;
+if (c > Max) Max = c;
+Console.WriteLine("max = " + Max);
