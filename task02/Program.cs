baf569dc3e0831bc184729c83a140292c7d182ac
Guryanov <sanyaguru@gmail.com>
2023-09-24 18:35:08 +0800

    fix task 02

--- task02/Program.cs
+++ task02/Program.cs
@@ -10,12 +10,16 @@ int a = int.Parse(Console.ReadLine()!);
 Console.Write("Введите число b: ");
 int b = int.Parse(Console.ReadLine()!);
 int Max = a;
+int Min = b;
 if (a > b)
 {
     Console.WriteLine("max = " + Max);
+    Console.WriteLine("min = " + Min);
 }
 else 
 {
     Max = b;
+    Min = a;
     Console.WriteLine("max = " + Max);
+    Console.WriteLine("min = " + Min);
 }
